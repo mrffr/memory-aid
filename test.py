23b7156145ff0a129ae22dd7622a8331684deac3
F.Fitzgerald <mrffitzgerald@runbox.com>
2019-05-15 17:10:53 +0100

    added tests file and renamed main file

---
+++ test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import unittest
+import memory_aid
+
+class TestMemoryAid(unittest.TestCase):
+    def test_pass(self):
+        self.assertEqual(1, 1)
+
+if __name__ == '__main__':
+    unittest.main()
