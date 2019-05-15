392397ebcd90c3008e045671fe311943a5426cdf
F.Fitzgerald <mrffitzgerald@runbox.com>
2019-05-15 17:58:02 +0100

    import questions from csv file

--- memory_aid.py
+++ memory_aid.py
@@ -1,5 +1,44 @@
 #!/usr/bin/env python3
 
+import sys
+import csv
+
+# import questions from csv file and return list of questions
+# questions are in format: q,a
+def import_questions(fname):
+    print("Importing questions from", fname)
+
+    try:
+        f = open(fname, 'r')
+    except OSError as e:
+        print("Import failed:", e)
+        return None
+
+    csvr = csv.reader(f)
+
+    # import each question
+    questions = []
+    count = 0
+    for line in csvr:
+        # question format is q, a
+        if len(line) != 2:
+            continue
+        # found a question
+        count += 1
+        q, a = line
+        print("Found:", q, a)
+
+        questions.append([q,a])
+
+    f.close()
+
+    print("Import successful:", count, "new questions found.")
+
+    return questions
+
+def export_questions(fname):
+    pass
+
 def main():
     pass
 
--- test.py
+++ test.py
@@ -7,5 +7,10 @@ class TestMemoryAid(unittest.TestCase):
     def test_pass(self):
         self.assertEqual(1, 1)
 
+    def test_import(self):
+        self.assertEqual(memory_aid.import_questions('not_a_file'), None)
+        self.assertNotEqual(memory_aid.import_questions('import_test.csv'), None)
+
+
 if __name__ == '__main__':
     unittest.main()
