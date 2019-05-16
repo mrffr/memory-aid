ffd42cfd4940c269da3ea3444f02acdf9a1c1b0d
F.Fitzgerald <mrffitzgerald@runbox.com>
2019-05-16 13:43:51 +0100

    added question class and deck class

--- memory_aid.py
+++ memory_aid.py
@@ -2,6 +2,27 @@
 
 import sys
 import csv
+import datetime
+
+class question:
+    def __init__(self, q, a, next_time,
+                 interval = 1, ease = 2.5,
+                 times_answered = 0, tags = []):
+        self.question = q
+        self.answer = a
+
+        self.next_time = next_time
+        self.interval = interval
+        self.ease = ease
+
+        self.times_answered = times_answered
+        self.tags = tags
+
+
+class deck:
+    def __init__(self):
+        self.questions = []
+
 
 # import questions from csv file and return list of questions
 # questions are in format: q,a
@@ -16,19 +37,23 @@ def import_questions(fname):
 
     csvr = csv.reader(f)
 
+    # get the time now
+    now = datetime.datetime.now()
+
     # import each question
     questions = []
     count = 0
     for line in csvr:
-        # question format is q, a
-        if len(line) != 2:
+        # question format is q, a, [tags, ]
+        if len(line) < 2 or len(line) > 3:
             continue
         # found a question
         count += 1
-        q, a = line
-        print("Found:", q, a)
+        q, a, *tags = line
+        print("Found:", q, a, tags)
 
-        questions.append([q,a])
+        new_q = question(q, a, now, tags = tags)
+        questions.append(new_q)
 
     f.close()
 
--- test.py
+++ test.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
 
 import unittest
-import memory_aid
+import memory_aid as ma
+import datetime
 
 class TestMemoryAid(unittest.TestCase):
     def test_pass(self):
         self.assertEqual(1, 1)
 
     def test_import(self):
-        self.assertEqual(memory_aid.import_questions('not_a_file'), None)
-        self.assertNotEqual(memory_aid.import_questions('import_test.csv'), None)
+        self.assertEqual(ma.import_questions('not_a_file'), None)
+        self.assertNotEqual(ma.import_questions('import_test.csv'), None)
+
+    def test_deck(self):
+        d = ma.deck()
+        self.assertEqual(d.questions, [])
+
+    def test_question(self):
+        now = datetime.datetime.now()
+        q = ma.question("q", "a", now)
+        self.assertEqual(q.question, "q")
+        self.assertEqual(q.answer, "a")
+        self.assertEqual(q.next_time, now)
 
 
 if __name__ == '__main__':
