db548656fed73cc16ea59901219490b99e66a52a
F.Fitzgerald <mrffitzgerald@runbox.com>
2019-05-16 13:48:40 +0100

    removed deck class as it's not needed

--- memory_aid.py
+++ memory_aid.py
@@ -19,11 +19,6 @@ class question:
         self.tags = tags
 
 
-class deck:
-    def __init__(self):
-        self.questions = []
-
-
 # import questions from csv file and return list of questions
 # questions are in format: q,a
 def import_questions(fname):
@@ -42,26 +37,31 @@ def import_questions(fname):
 
     # import each question
     questions = []
-    count = 0
     for line in csvr:
         # question format is q, a, [tags, ]
         if len(line) < 2 or len(line) > 3:
             continue
+
         # found a question
-        count += 1
         q, a, *tags = line
         print("Found:", q, a, tags)
 
+        # create question and add to question list
         new_q = question(q, a, now, tags = tags)
         questions.append(new_q)
 
     f.close()
 
-    print("Import successful:", count, "new questions found.")
+    print("Import successful:", len(questions), "new questions found.")
 
     return questions
 
+# export questions to json file
 def export_questions(fname):
+    pass
+
+# load questions from json file
+def load_questions(fname):
     pass
 
 def main():
