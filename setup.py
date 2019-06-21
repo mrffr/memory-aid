99ab4f39b91e8f519abb59498c932a054a96b49a
F.Fitzgerald <mrffitzgerald@runbox.com>
2019-06-21 06:57:09 +0100

    Packaging setup.

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open("README.md",'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='memory-aid-mrffr',
+    version="0.0.1",
+    description='CLI utility to aid in memorizing facts using spaced repetition.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='F Fitzgerald',
+    url='https://github.com/mrffr/memory-aid',
+
+    # the directory name manually specified to avoid including tests
+    packages=['memory_aid'], 
+    # identifiers for pypi
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
